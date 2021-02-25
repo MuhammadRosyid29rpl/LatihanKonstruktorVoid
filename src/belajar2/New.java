9f74094ee749f8bd832fc880dc631c3754dfa7a7
MuhammadRosyid29rpl <muhammad_rosyid_29rpl@student.smktelkom-mlg.sch.id>
2021-02-25 20:48:00 +0700

    void

---
+++ src/belajar2/New.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package belajar2;
+
+/**
+ *
+ * @author ASUS TUF A15
+ */
+public class New {
+    int sisi;
+    int hasil;
+
+void New (int sisi){
+    this.sisi = sisi;
+}
+    void Hitung(){
+        hasil=4*sisi;
+        System.out.println("Keliling Persegi adalah : "+hasil);
+}
+}
+
---
+++ src/belajar2/Void.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package belajar2;
+
+/**
+ *
+ * @author ASUS TUF A15
+ */
+public class Void {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    New objek = new New();
+    objek.New(5);
+    objek.Hitung();
+    }
+    
+}
